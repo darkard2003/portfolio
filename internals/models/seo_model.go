ce9ffc5e0fb833ef7d0546d3a678580f4813c73e
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2025-12-25 02:01:21 +0530

    feat: Added SEO models and components

---
+++ internals/models/seo_model.go
@@ -0,0 +1,65 @@
+package models
+
+type SEOModel struct {
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	Url         string       `json:"url"`
+	Robots      string       `json:"robots"`
+	OG          OGModel      `json:"og"`
+	Twitter     TwitterModel `json:"twitter"`
+}
+
+func NewSEOModel(
+	title,
+	description,
+	url,
+	robots,
+	ogtype,
+	image string,
+) SEOModel {
+	ogModel := NewOGModel(ogtype, url, title, description, image)
+	twitterModel := NewTwitterModel("summary", title, description, image)
+	return SEOModel{
+		Title:       title,
+		Description: description,
+		Url:         url,
+		Robots:      robots,
+		OG:          ogModel,
+		Twitter:     twitterModel,
+	}
+}
+
+type OGModel struct {
+	Type        string `json:"type"`
+	Url         string `json:"url"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+}
+
+func NewOGModel(ogType, url, title, description, image string) OGModel {
+	return OGModel{
+		Type:        ogType,
+		Url:         url,
+		Title:       title,
+		Description: description,
+		Image:       image,
+	}
+
+}
+
+type TwitterModel struct {
+	Card        string `json:"card"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+}
+
+func NewTwitterModel(card, title, description, image string) TwitterModel {
+	return TwitterModel{
+		Card:        card,
+		Title:       title,
+		Description: description,
+		Image:       image,
+	}
+}
